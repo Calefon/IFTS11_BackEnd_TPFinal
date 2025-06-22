bb2732947e8e618e8451be3c723b74da05fe2c0f
Ezequiel <eze5025@gmail.com>
2025-06-22 01:47:12 -0300

    agregado getAllStickies en controller

---
+++ src/controllers/stickiesController.js
@@ -0,0 +1,12 @@
+import * as stickiesService from '../services/stickiesService.js'
+
+const getAllStickies = async (req, res) => {
+    try{
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200);
+        res.send(await stickiesService.getAllStickies());
+    }catch(error){
+        console.error(`CONTROLLER ERROR - Error al intentar buscar todas las stickies: \n***\n\t${error}`);
+        res.status(500).send({ code: 500, message: "Error al intentar obtener todas las stickies"});
+    }
+}
