b24beafb2144ae9d1e14bec849edfde123cb4a1f
Kevin Maximiliano Palma Romero <kev.mpr03@gmail.com>
2025-06-24 12:53:02 -0300

    feat(listas): add PUT endpoint to update list title including controller, service, and router with error handling

--- src/db/mongoDBConection.js
+++ src/db/mongoDBConection.js
@@ -1,20 +1,26 @@
 import { configMongoDB } from "../config/config.js";
 import mongoose from "mongoose";
 
-const defaultURI = `mongodb+srv://zoevivas64:Kc4wZv9gpTSjYtTa@cluster0.9kqfxnf.mongodb.net/`;
+const defaultURI1 = `mongodb+srv://zoevivas64:Kc4wZv9gpTSjYtTa@cluster0.9kqfxnf.mongodb.net/`;
+const defaultURI = `mongodb+srv://kevmpr:AlNWvBRWkyy3UrHd@mi-cluster-kevin.zxo5ujl.mongodb.net/kanban?retryWrites=true&w=majority&appName=mi-cluster-kevin`;
 
-const URI = configMongoDB.useCustomURI ? configMongoDB.customMongoDBURI : defaultURI;
+const URI = configMongoDB.useCustomURI
+  ? configMongoDB.customMongoDBURI
+  : defaultURI;
 
 export const connectToMongoDBMongoose = async () => {
-    console.log(configMongoDB.useCustomURI)
-    console.log(URI)
-    try {
-        await mongoose.connect(URI);
-        console.log(`MongoDB connection succesfull.\nCluster: ${configMongoDB.cluster}`)
-    } catch (error) {
-        console.log(`Error trying to connect to MongoDB database. Cluster: ${configMongoDB.cluster}`)
-        console.error(error);
-        process.exit(1);
-    }
-
-}
+  console.log(configMongoDB.useCustomURI);
+  console.log(URI);
+  try {
+    await mongoose.connect(URI);
+    console.log(
+      `MongoDB connection succesfull.\nCluster: ${configMongoDB.cluster}`
+    );
+  } catch (error) {
+    console.log(
+      `Error trying to connect to MongoDB database. Cluster: ${configMongoDB.cluster}`
+    );
+    console.error(error);
+    process.exit(1);
+  }
+};
